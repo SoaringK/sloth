f49bf715ae992c6fe8b4b0ecfe85e6a4422f372d
roffen <ohroffen@foxmail.com>
2018-12-09 23:15:15 +0800

    防止地址填写数据空值

--- client/pages/address_add/address_add.js
+++ client/pages/address_add/address_add.js
@@ -44,54 +44,82 @@ Page({
   },
   formSubmit: function (e) {
     var that = this;
-    console.log(e)
-    wx.request({
-      // building信息保存的是下标
-      url: config.service.addAddressUrl + "?cust_name=" + e.detail.value.namearea + "&cust_wechat="+e.detail.value.wechatarea+"&cust_phone=" + e.detail.value.phonearea + "&cust_addr_room=" + e.detail.value.addressarea +"&user_id="+that.data.userId+ "&cust_addr_building="+that.data.dormitory_index,
-      method: "GET",
-      header: {
-        "content-type": "application/x-wwww-form-urlencoded"
-      },
-      success: function (res) {
-        console.log(res)
-        // 因为路由部分修改成navigatorBack，为了地址页面实时更新，用getCurrentPages实现
-        var address = {
-          cust_id: that.data.userId,
-          cust_name: e.detail.value.namearea,
-          cust_phone: e.detail.value.phonearea,
-          cust_addr_room: e.detail.value.addressarea,
-          cust_Wechat: e.detail.value.wechatarea,
-          cust_addr_building:that.data.dormitory_index,
-          default_id:0,
-          addr_id:0//稍后修改
-        };
-        var pages = getCurrentPages();
-        var prevPage = pages[pages.length-2];
-        address.addr_id = prevPage.data.customer.length;
-        var customer = prevPage.data.customer;
-        customer.push(address);
-        prevPage.setData({
-          customer:customer,
-          m: 1
-        });
-        wx.showToast({
-          title: '新增地址成功',
-          icon: 'success',
-          duration: 2000
-        })
-        setTimeout(function (e) {
-          // 成功后返回上一页
-          wx.navigateBack({
-            delta:1
+    //防止信息为空
+    if (e.detail.value.namearea == "") {
+      wx.showModal({
+        title: '提示',
+        content: "请填写您的姓名！"
+      })
+    }
+    else if (e.detail.value.phonearea == "") {
+      wx.showModal({
+        title: '提示',
+        content: "请填写您的手机号！"
+      })
+    }
+    else if (e.detail.value.addressarea == "") {
+      wx.showModal({
+        title: '提示',
+        content: "请输入您的宿舍号！"
+      })
+    }
+    else if (e.detail.value.wechatarea == "") {
+      wx.showModal({
+        title: '提示',
+        content: "请输入您的微信号！"
+      })
+    }
+    else
+    {
+      // console.log(e)
+      wx.request({
+        // building信息保存的是下标
+        url: config.service.addAddressUrl + "?cust_name=" + e.detail.value.namearea + "&cust_wechat="+e.detail.value.wechatarea+"&cust_phone=" + e.detail.value.phonearea + "&cust_addr_room=" + e.detail.value.addressarea +"&user_id="+that.data.userId+ "&cust_addr_building="+that.data.dormitory_index,
+        method: "GET",
+        header: {
+          "content-type": "application/x-wwww-form-urlencoded"
+        },
+        success: function (res) {
+          console.log(res)
+          // 因为路由部分修改成navigatorBack，为了地址页面实时更新，用getCurrentPages实现
+          var address = {
+            cust_id: that.data.userId,
+            cust_name: e.detail.value.namearea,
+            cust_phone: e.detail.value.phonearea,
+            cust_addr_room: e.detail.value.addressarea,
+            cust_Wechat: e.detail.value.wechatarea,
+            cust_addr_building:that.data.dormitory_index,
+            default_id:0,
+            addr_id:0//稍后修改
+          };
+          var pages = getCurrentPages();
+          var prevPage = pages[pages.length-2];
+          address.addr_id = prevPage.data.customer.length;
+          var customer = prevPage.data.customer;
+          customer.push(address);
+          prevPage.setData({
+            customer:customer,
+            m: 1
+          });
+          wx.showToast({
+            title: '新增地址成功',
+            icon: 'success',
+            duration: 2000
           })
-        }, 2000)
-      }
-      /*data:{
-        cust_name:e.detail.value.namearea,
-        cust_phone:e.detail.value.phonearea,
-        cust_addr:e.detail.value.addressarea
-      }*/
-    })
+          setTimeout(function (e) {
+            // 成功后返回上一页
+            wx.navigateBack({
+              delta:1
+            })
+          }, 2000)
+        }
+        /*data:{
+          cust_name:e.detail.value.namearea,
+          cust_phone:e.detail.value.phonearea,
+          cust_addr:e.detail.value.addressarea
+        }*/
+      })
+    }
   },
   dormitory_change: function(e){
     // console.log(e.detail.value);
